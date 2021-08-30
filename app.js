0c2c2adfef2cf45e704c20fb603eb00905066cc7
Tim <s87462830@gmail.com>
2021-08-30 22:38:01 +0800

    feat: Add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Record = require('./models/record')//load Record model
 
 const port = 3000
 const app = express()
 
+//------------ database ------------//
+mongoose.connect('mongodb://localhost/Expense', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('Mongodb error')
+})
+
+db.once('open', () => {
+  console.log('Mongodb is connected!')
+})
+//------------ database ------------//
+
+//Setting Routes
 app.get('/', (req, res) => {
   res.send('test!')
 })
---
+++ models/record.js
@@ -0,0 +1,30 @@
+//load mongoose
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+//create Schema
+const recordSchema = new Schema({
+  id: {
+    type: Number,
+    require: true,
+  },
+  name: {
+    type: String,
+    require: true,
+  },
+  date: {
+    type: Number,
+    require: true,
+  },
+  amount: {
+    type: Number,
+    require: true,
+  },
+  category: {
+    type: String,
+    require: true,
+  },
+})
+
+//output model
+module.exports = mongoose.model('Record', recordSchema)
