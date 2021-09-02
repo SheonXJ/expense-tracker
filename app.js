4e832738a9af6d919f2c231a4483888db8de6f7a
Tim <s87462830@gmail.com>
2021-09-02 22:27:24 +0800

    feat: Add method-override & Add routes(create,edit,delete)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const Record = require('./models/record')//load Record model
+const methodOverride = require('method-override')
 const exhbs = require('express-handlebars')
 
 const port = 3000
@@ -19,13 +20,21 @@ db.once('open', () => {
 })
 //------------ database ------------//
 
+
 //------------ template engine ------------//
 app.engine('hbs', exhbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 //------------ template engine ------------//
 
 
-//Setting Routes
+//------------ Setting ------------//
+//Setting method-override 路由覆蓋機制
+app.use(methodOverride('_method'))
+//------------ Setting ------------//
+
+
+//------------ Setting Routes ------------//
+//Route: index page
 app.get('/', (req, res) => {
   Record.find()
   .lean()
@@ -36,6 +45,28 @@ app.get('/', (req, res) => {
   })
   .catch(error => console.log(error))
 })
+
+//Route: edit page
+app.get('/expenseTracker/:id/edit', (req, res) => {
+  Record.findOne({id: req.params.id})
+    .lean()
+    .then(record => res.render('edit', {record}))
+    .catch(error => console.log(error))
+})
+
+//Route: create page
+app.get('/expenseTracker/create', (req, res) => {
+  res.render('create')
+})
+
+//Route: delete record data
+app.delete('/expenseTracker/:id', (req, res) => {
+  return Record.deleteOne({id: req.params.id})
+  .then(() => res.redirect('/'))
+  .catch(error => console.log(error))
+})
+//------------ Setting Routes ------------//
+
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
