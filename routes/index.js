cf69367bc00b9dec56afe123ed2c7eb10253f6e2
Tim <s87462830@gmail.com>
2021-10-24 23:15:39 +0800

    feat: add user isAuthenticated in home and expenseTracker routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const router = express.Router()
 
+const { authenticator } = require('../middleware/auth')
+
 // 引入模組程式碼
 // 將網址結構符合字串的 request 導向模組 
 const home = require('./modules/home')
@@ -9,8 +11,8 @@ const users = require('./modules/users')
 const expenseTracker = require('./modules/expenseTracker')
 
 router.use('/users', users)
-router.use('/expenseTracker', expenseTracker)
-router.use('/', home)
+router.use('/expenseTracker', authenticator, expenseTracker)
+router.use('/', authenticator, home)
 
 // 匯出路由器
 module.exports = router
