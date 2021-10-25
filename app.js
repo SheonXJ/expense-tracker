f6e68c55b02857bc724effef5703fa90fdac76d9
Tim <s87462830@gmail.com>
2021-10-25 21:30:49 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const methodOverride = require('method-override')
 const exhbs = require('express-handlebars')
 const session = require('express-session')
+const flash = require('connect-flash')
 const routes = require('./routes')
 
 const usePassport = require('./config/passport')
@@ -32,10 +33,15 @@ app.use(express.static('public'))
 app.use(methodOverride('_method'))
 //Setting body-parser 進行前置處理
 app.use(express.urlencoded({ extended: true }))
+//Setting flash message
+app.use(flash())
 //Setting get user data & authenticated state to hbs
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated
   res.locals.user = req.user
+  //get flash message to hbs
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 // 將 request 導入路由器
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -19,11 +19,29 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const {name, email, password, confirmPassword} = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+
   User.findOne({ email })
     .then(user => {
       if(user) {
-        console.log('這個 Email 已經註冊過了。')
+        errors.push({ message: '這個 Email 已經註冊過了。' })
         return res.render('register', {
+          errors,
           name,
           email,
           password,
@@ -41,6 +59,7 @@ router.post('/register', (req, res) => {
 })
 
 router.get('/logout', (req, res) => {
+  req.flash('success_msg', '您已成功登出!')
   req.logout()
   res.redirect('/users/login')
 })
